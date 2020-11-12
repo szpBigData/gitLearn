8d056222bdb5bd3511aa9baf8b807e4e66a13630
szp <sunzhipeng96@163.com>
2020-11-12 20:18:35 +0800

    Initial commit

---
+++ src/main/java/com/test/jianfa/Dugu9jian.java
@@ -0,0 +1,11 @@
+package com.test.jianfa;
+
+/**
+ * @author sunzhipeng
+ * @create 2020-11-12 20:17
+ */
+public class Dugu9jian {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
